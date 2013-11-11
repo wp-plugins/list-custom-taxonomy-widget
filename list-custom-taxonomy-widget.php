52d28669dfad7ba0b3e9fee9f9e230bb7e3d9a7a
celloexpressions <celloexpressions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-11 02:37:24 +0000

    Try another fix for the long-broken dropdown option. This appears to work for both hierarchical and non-hierarchical custom taxonomies, but still breaks for built-in ones.
    git-svn-id: https://plugins.svn.wordpress.org/list-custom-taxonomy-widget/trunk@802221 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- list-custom-taxonomy-widget.php
+++ list-custom-taxonomy-widget.php
@@ -295,8 +295,9 @@ class lctwidget_Taxonomy_Dropdown_Walker extends Walker {
 	var $tree_type = 'category';
 	var $db_fields = array ( 'id' => 'term_id', 'parent' => 'parent' );
 
-		function start_el( &$output, $term, $depth = 0, $args = array(), $current_object_id = 0 ) {
-		$url = get_term_link( $term, $term->taxonomy );
+	function start_el( &$output, $term, $depth = 0, $args = array(), $current_object_id = 0 ) {
+		$term = get_term( $term, $term->taxonomy );
+		$term_slug = $term->slug;
 
 		$text = str_repeat( '&nbsp;', $depth * 3 ) . $term->name;
 		if ( $args['show_count'] ) {
@@ -305,7 +306,7 @@ class lctwidget_Taxonomy_Dropdown_Walker extends Walker {
 
 		$class_name = 'level-' . $depth;
 
-		$output.= "\t" . '<option' . ' class="' . esc_attr( $class_name ) . '" value="' . esc_url( $url ) . '">' . esc_html( $text ) . '</option>' . "\n";
+		$output.= "\t" . '<option' . ' class="' . esc_attr( $class_name ) . '" value="' . esc_attr( $term_slug ) . '">' . esc_html( $text ) . '</option>' . "\n";
 	}
 }
 ?>
